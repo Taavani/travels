6d96b9a0c5a123a1422d3a3a1443b3924894ae84
Jóhan H Dam-Davidsen <johan.davidsen@fjakkarin.com>
2021-05-20 12:45:30 -0200

    Fixed lightgallery

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -4,6 +4,7 @@
 
 import 'jquery'
 import 'rd-navbar/dist/js/jquery.rd-navbar';
+import lightGallery from 'lightgallery';
 
 $(document).ready(() => {
 
@@ -44,9 +45,9 @@ $(document).ready(() => {
         // isotope: $('.isotope'),
         // popover: $('[data-toggle="popover"]'),
         // viewAnimate: $('.view-animate'),
-        // lightGallery: $('[data-lightgallery="group"]'),
-        // lightGalleryItem: $('[data-lightgallery="item"]'),
-        // lightDynamicGalleryItem: $('[data-lightgallery="dynamic"]'),
+        lightGallery: $('[data-lightgallery="group"]'),
+        lightGalleryItem: $('[data-lightgallery="item"]'),
+        lightDynamicGalleryItem: $('[data-lightgallery="dynamic"]'),
         // radio: $('input[type="radio"]'),
         // checkbox: $('input[type="checkbox"]'),
         // customToggle: $('[data-custom-toggle]'),
@@ -142,6 +143,100 @@ $(document).ready(() => {
 
         if (plugins.rdNavbar.attr("data-body-class")) {
             document.body.className += ' ' + plugins.rdNavbar.attr("data-body-class");
+        }
+    }
+
+    /**
+     * @desc Initialize the gallery with set of images
+     * @param {object} itemsToInit - jQuery object
+     * @param {string} addClass - additional gallery class
+     */
+    function initLightGallery(itemsToInit, addClass) {
+        $(itemsToInit).lightGallery({
+            thumbnail: $(itemsToInit).attr("data-lg-thumbnail") !== "false",
+            selector: "[data-lightgallery='item']",
+            autoplay: $(itemsToInit).attr("data-lg-autoplay") === "true",
+            pause: parseInt($(itemsToInit).attr("data-lg-autoplay-delay")) || 5000,
+            addClass: addClass,
+            mode: $(itemsToInit).attr("data-lg-animation") || "lg-slide",
+            loop: $(itemsToInit).attr("data-lg-loop") !== "false",
+            showThumbByDefault: false
+        });
+    }
+
+    /**
+     * @desc Initialize the gallery with dynamic addition of images
+     * @param {object} itemsToInit - jQuery object
+     * @param {string} addClass - additional gallery class
+     */
+    function initDynamicLightGallery(itemsToInit, addClass) {
+        $(itemsToInit).on("click", function () {
+            $(itemsToInit).lightGallery({
+                thumbnail: $(itemsToInit).attr("data-lg-thumbnail") !== "false",
+                selector: "[data-lightgallery='item']",
+                autoplay: $(itemsToInit).attr("data-lg-autoplay") === "true",
+                pause: parseInt($(itemsToInit).attr("data-lg-autoplay-delay")) || 5000,
+                addClass: addClass,
+                mode: $(itemsToInit).attr("data-lg-animation") || "lg-slide",
+                loop: $(itemsToInit).attr("data-lg-loop") !== "false",
+                dynamic: true,
+                dynamicEl: JSON.parse($(itemsToInit).attr("data-lg-dynamic-elements")) || []
+            });
+        });
+    }
+
+    /**
+     * @desc Initialize the gallery with one image
+     * @param {object} itemToInit - jQuery object
+     * @param {string} addClass - additional gallery class
+     */
+    function initLightGalleryItem(itemToInit, addClass) {
+        $(itemToInit).lightGallery({
+            selector: "this",
+            addClass: addClass,
+            counter: false,
+            youtubePlayerParams: {
+                modestbranding: 1,
+                showinfo: 0,
+                rel: 0,
+                controls: 0
+            },
+            vimeoPlayerParams: {
+                byline: 0,
+                portrait: 0
+            }
+        });
+    }
+
+    // lightGallery
+    if (plugins.lightGallery.length) {
+        for (var i = 0; i < plugins.lightGallery.length; i++) {
+            initLightGallery(plugins.lightGallery[i]);
+        }
+    }
+
+    // lightGallery item
+    if (plugins.lightGalleryItem.length) {
+        // Filter carousel items
+        var notCarouselItems = [];
+
+        for (var z = 0; z < plugins.lightGalleryItem.length; z++) {
+            if (!$(plugins.lightGalleryItem[z]).parents('.owl-carousel').length && !$(plugins.lightGalleryItem[z]).parents('.swiper-slider').length && !$(plugins.lightGalleryItem[z]).parents('.slick-slider').length) {
+                notCarouselItems.push(plugins.lightGalleryItem[z]);
+            }
+        }
+
+        plugins.lightGalleryItem = notCarouselItems;
+
+        for (var i = 0; i < plugins.lightGalleryItem.length; i++) {
+            initLightGalleryItem(plugins.lightGalleryItem[i]);
+        }
+    }
+
+    // Dynamic lightGallery
+    if (plugins.lightDynamicGalleryItem.length) {
+        for (var i = 0; i < plugins.lightDynamicGalleryItem.length; i++) {
+            initDynamicLightGallery(plugins.lightDynamicGalleryItem[i]);
         }
     }
 
